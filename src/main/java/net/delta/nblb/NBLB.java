5c21b53f1ae10cabbbd15b153047fbd2456acd85
DeltaV <105618876+GitsurferQwerty@users.noreply.github.com>
2025-06-08 11:14:59 -0600

    Initial commit

---
+++ src/main/java/net/delta/nblb/NBLB.java
@@ -0,0 +1,16 @@
+package net.delta.nblb;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NBLB implements ModInitializer {
+	public static final String MOD_ID = "nblb";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/delta/nblb/NBLBDataGenerator.java
@@ -0,0 +1,11 @@
+package net.delta.nblb;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NBLBDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
